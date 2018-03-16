cfd97249d07d9478446d407d80d9512725a04a78
armdong <armdong@163.com>
2018-03-16 10:13:03 +0800

    adding animations

--- public/assets/js/inbox.js
+++ public/assets/js/inbox.js
@@ -5,10 +5,75 @@
       anchors: ['1st-page', '2nd-page', '3rd-page', '4th-page', '5th-page', '6th-page'],
       navigation: true,
       avigationPosition: 'right',
-      responsiveWidth: 320,
-      responsiveHeight: 400,
-      afterResponsive: function(isResponsive) {
+      afterLoad: function(anchorLink, index) {
+        var $section = $(this);
 
+        if (index === 1) {
+          $section
+            .find('.section__img')
+            .addClass('animated')
+            .addClass('rollIn')
+            .show();
+
+          $section
+            .find('.section__logo')
+            .addClass('animated')
+            .addClass('slideInRight')
+            .show();
+        } else {
+          $section
+            .find('.section__text')
+            .addClass('animated')
+            .addClass('swing')
+            .show();
+
+          $section
+            .find('.section__box')
+            .addClass('animated')
+            .addClass('bounceInDown')
+            .show();
+
+          $section
+            .find('.section__cake')
+            .addClass('animated')
+            .addClass('lightSpeedIn')
+            .show();
+        }
+      },
+      onLeave: function(index, nextIndex, direction) {
+        var $section = $(this);
+
+        if (index == 1) {
+          $section
+            .find('.section__img')
+            .removeClass('animated')
+            .removeClass('rollIn')
+            .fadeOut();
+
+          $section
+            .find('.section__logo')
+            .removeClass('animated')
+            .removeClass('slideInRight')
+            .fadeOut();
+        } else {
+          $section
+            .find('.section__text')
+            .removeClass('animated')
+            .removeClass('swing')
+            .fadeOut();
+
+          $section
+            .find('.section__box')
+            .removeClass('animated')
+            .removeClass('bounceInDown')
+            .fadeOut();
+
+          $section
+            .find('.section__cake')
+            .removeClass('animated')
+            .removeClass('lightSpeedIn')
+            .fadeOut();
+        }
       }
     });
 
